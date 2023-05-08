f141b6322b499075450cf11d8b61c60dd72356b8
Victor Porton <porton.victor@gmail.com>
2023-05-08 22:15:27 +0300

    caninsters renamed

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-const frontendDirectory = "salaries_science_frontend";
+const frontendDirectory = "salaries_frontend";
 
 const frontend_entry = path.join("src", frontendDirectory, "src", "index.html");
 
