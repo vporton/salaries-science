199fa3a01268a0beb0e9e76950632737084a5b38
Victor Porton <porton.victor@gmail.com>
2025-08-02 04:14:19 +0300

    fixing compilation

--- science-grants-blockchain/vite.config.ts
+++ science-grants-blockchain/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import environment from 'vite-plugin-environment';
 
 export default defineConfig({
-  plugins: [react()],
   root: './src/frontend',
   build: {
     outDir: '../../dist',
@@ -15,5 +15,12 @@ export default defineConfig({
         changeOrigin: true,
       }
     }
-  }
+  },
+  plugins: [
+    react(),
+    // nodePolyfills(),
+    environment("all", { prefix: "CANISTER_", defineOn: 'process.env' }),
+    environment("all", { prefix: "DFX_", defineOn: 'process.env' }),
+    environment(["II_URL"]),
+  ],
 })
